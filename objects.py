3b4577d20ef9ffd8151e88ffba8893112cd95e5d
elvgarrui <elvgarrui@alum.us.es>
2018-03-01 19:32:12 +0100

    Variables refactorizadas

--- objects.py
+++ objects.py
@@ -1,20 +1,20 @@
 
 class GeneralInfo:
     def __init__(self, R:int, C:int, F:int, N:int, B:int, T:int):
-        self.R = R
-        self.C = C
-        self.F = F
-        self.N = N
-        self.B = B
-        self.T = T
+        self.row = R
+        self.column = C
+        self.float = F
+        self.rides = N
+        self.bonus = B
+        self.steps = T
 
 
 class Ride:
     def __init__(self, i:int, a:int, b:int, x:int, y:int, s:int, f:int):
         self.i = i
-        self.a = a
-        self.b = b
-        self.x = x
-        self.y = y
-        self.s = s
-        self.f = f
+        self.startrow = a
+        self.startcolumn = b
+        self.finishrow = x
+        self.finishcolumn = y
+        self.earlystart = s
+        self.latestfinish = f
